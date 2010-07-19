a3a1a968c9ab0a412ab2e64287a029aad7834c0b
Joseph Daigle <joseph@cridion.com>
2010-07-19 13:03:51 -0400

    example component 'mapping'

--- SimpleQuery.Example.ViewModel/CustomerReport.cs
+++ SimpleQuery.Example.ViewModel/CustomerReport.cs
@@ -8,5 +8,13 @@ namespace SimpleQuery.Example.ViewModel {
         public string CustomerId { get; set; }
         public string Name { get; set; }
         public string PhoneNumber { get; set; }
+        public Address Address { get; set; }
+    }
+
+    public class Address {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string PostalCode { get; set; }
     }
 }
